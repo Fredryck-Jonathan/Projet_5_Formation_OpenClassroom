0c1d0d82ee7a96d68ed9ce8cbe29a8454bda0f58
Fredryck-Jonathan <102037006+Fredryck-Jonathan@users.noreply.github.com>
2022-08-01 00:15:31 +0200

    test try et catch sur script.js non fonctionnel

--- front/js/script.js
+++ front/js/script.js
@@ -1,65 +1,164 @@
-let promise01 = fetch("http://localhost:3000/api/products");
-let items = document.getElementById("items")
+// let promise01 = fetch("http://localhost:3000/api/products");
+// let items = document.getElementById("items")
 
- promise01 
-    .then((response) => {
+ // promise01 
+    // .then((response) => {
         
-        const productsData = response.json();
+        // const productsData = response.json();
     
-        productsData.then((products) => {
-            for (product in products){
-                let lien = document.createElement("a")
-                let href = document.createAttribute("href")
-                href.value = "./product.html?id="+ products[product]._id;
-                lien.setAttributeNode(href)
+        // productsData.then((products) => {
+            // for (product of products){
+                // let lien = document.createElement("a")
+                // let href = document.createAttribute("href")
+                // href.value = "./product.html?id="+ product._id;
+                // lien.setAttributeNode(href)
 
 
-                let article = document.createElement("article")
+                // let article = document.createElement("article")
 
 
-                let img = document.createElement("img")
-                let src = document.createAttribute("src")
-                src.value = products[product].imageUrl
+                // let img = document.createElement("img")
+                // let src = document.createAttribute("src")
+                // src.value = product.imageUrl
     
-                let alt = document.createAttribute("alt")
-                alt.value = products[product].altTxt
+                // let alt = document.createAttribute("alt")
+                // alt.value = product.altTxt
 
-                img.setAttributeNode(src)
-                img.setAttributeNode(alt)
+                // img.setAttributeNode(src)
+                // img.setAttributeNode(alt)
 
             
-                let titre = document.createElement("h3")
-                let classeNom = document.createAttribute("class")
-                classeNom.value = "productName"
-                titre.textContent = products[product].name
+                // let titre = document.createElement("h3")
+                // let classeNom = document.createAttribute("class")
+                // classeNom.value = "productName"
+                // titre.textContent = product.name
 
-                titre.setAttributeNode(classeNom)
+                // titre.setAttributeNode(classeNom)
 
-                let para = document.createElement("p")
-                let classePara = document.createAttribute("class")
-                classePara.value = "productDescription"
-                para.textContent = products[product].description
+                // let para = document.createElement("p")
+                // let classePara = document.createAttribute("class")
+                // classePara.value = "productDescription"
+                // para.textContent = product.description
                 
-                para.setAttributeNode(classePara)
+                // para.setAttributeNode(classePara)
 
-                lien.append(article)
-                article.append(img)
-                article.append(titre)
-                article.append(para)
-                items.append(lien)
-
-
-            console.log(href, products, src, alt, titre, para, img)
+                // lien.append(article)
+                // article.append(img)
+                // article.append(titre)
+                // article.append(para)
+                // items.append(lien)
 
 
+            // console.log(href, products, src, alt, titre, para, img)
 
 
-            }
-        });
+
+
+            // }
+        // });
 
        
-    })
+    // })
     
 
-const products = promise01
-console.log(products)
+// const products = promise01
+// console.log(products)
+
+function init() {
+    try{
+        fetch("http://localhost:3000/api/products")
+        
+        .then((response) => response.json())
+        .then((products) => {
+            turnOnProducts(products);
+        });
+    } catch(err){
+
+    alert("error")
+        
+    }
+}
+
+function turnOnProducts(products) {
+    let items = document.getElementById("items");
+    for (product of products){
+        let cardProduct = displayProduct(product);
+        items.append(cardProduct);
+    }
+}
+
+function displayProduct(product) {
+    let lien = document.createElement("a");
+    // let href = document.createAttribute("href");
+    // href.value = "./product.html?id="+ product._id;
+    // lien.setAttributeNode(href);
+    // peut être remplacé par, tu remarqueras le litéral de gabarit pour éviter une concaténation la variable est entre ${}
+    lien.setAttribute('href', `./product.html?id=${product._id}`);
+
+
+    let article = document.createElement("article");
+
+
+    let img = document.createElement("img");
+    // let src = document.createAttribute("src");
+    // src.value = product.imageUrl;
+
+    // let alt = document.createAttribute("alt");
+    // alt.value = product.altTxt;
+    // peut être remplacé par
+    img.setAttribute('src', product.imageUrl);
+    img.setAttribute('alt', product.altTxt);
+
+    // img.setAttributeNode(src);
+    // img.setAttributeNode(alt);
+
+
+    let titre = document.createElement("h3");
+    // let classeNom = document.createAttribute("class");
+    // classeNom.value = "productName";
+    // peut être remplacé par
+    titre.classList.add('productName');
+
+    titre.textContent = product.name;
+
+    // titre.setAttributeNode(classeNom);
+
+    let para = document.createElement("p");
+    // let classePara = document.createAttribute("class");
+    // classePara.value = "productDescription";
+    // peut être remplacé par
+    para.classList.add('productDescription');
+
+    para.textContent = product.description;
+    
+    // para.setAttributeNode(classePara);
+
+    lien.append(article);
+    // article.append(img);
+    // article.append(titre);
+    // article.append(para);
+    // peut être remplacé par
+    article.append(img, titre, para);
+
+    return lien;
+}
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+try{
+  init();
+} 
+catch(err) {
+    alert("ERROR : Veuillez contacter le service client")
+}
+
+
+
+});
+
+
+
+
+
+
+
