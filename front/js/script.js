a7a7c1d708844db2529790608da1850b164555b0
Fredryck-Jonathan <102037006+Fredryck-Jonathan@users.noreply.github.com>
2022-08-08 15:22:44 +0200

    suppression des commentaires

--- front/js/script.js
+++ front/js/script.js
@@ -1,82 +1,16 @@
-// let promise01 = fetch("http://localhost:3000/api/products");
-// let items = document.getElementById("items")
-
- // promise01 
-    // .then((response) => {
-        
-        // const productsData = response.json();
-    
-        // productsData.then((products) => {
-            // for (product of products){
-                // let lien = document.createElement("a")
-                // let href = document.createAttribute("href")
-                // href.value = "./product.html?id="+ product._id;
-                // lien.setAttributeNode(href)
-
-
-                // let article = document.createElement("article")
-
-
-                // let img = document.createElement("img")
-                // let src = document.createAttribute("src")
-                // src.value = product.imageUrl
-    
-                // let alt = document.createAttribute("alt")
-                // alt.value = product.altTxt
-
-                // img.setAttributeNode(src)
-                // img.setAttributeNode(alt)
-
-            
-                // let titre = document.createElement("h3")
-                // let classeNom = document.createAttribute("class")
-                // classeNom.value = "productName"
-                // titre.textContent = product.name
-
-                // titre.setAttributeNode(classeNom)
-
-                // let para = document.createElement("p")
-                // let classePara = document.createAttribute("class")
-                // classePara.value = "productDescription"
-                // para.textContent = product.description
-                
-                // para.setAttributeNode(classePara)
-
-                // lien.append(article)
-                // article.append(img)
-                // article.append(titre)
-                // article.append(para)
-                // items.append(lien)
-
-
-            // console.log(href, products, src, alt, titre, para, img)
-
-
-
-
-            // }
-        // });
-
-       
-    // })
-    
-
-// const products = promise01
-// console.log(products)
-
 function init() {
-    try{
+
         fetch("http://localhost:3000/api/products")
         
         .then((response) => response.json())
         .then((products) => {
             turnOnProducts(products);
-        });
-    } catch(err){
-
-    alert("error")
+            
         
-    }
+        });
+        
+
+ 
 }
 
 function turnOnProducts(products) {
@@ -89,10 +23,6 @@ function turnOnProducts(products) {
 
 function displayProduct(product) {
     let lien = document.createElement("a");
-    // let href = document.createAttribute("href");
-    // href.value = "./product.html?id="+ product._id;
-    // lien.setAttributeNode(href);
-    // peut être remplacé par, tu remarqueras le litéral de gabarit pour éviter une concaténation la variable est entre ${}
     lien.setAttribute('href', `./product.html?id=${product._id}`);
 
 
@@ -100,44 +30,24 @@ function displayProduct(product) {
 
 
     let img = document.createElement("img");
-    // let src = document.createAttribute("src");
-    // src.value = product.imageUrl;
 
-    // let alt = document.createAttribute("alt");
-    // alt.value = product.altTxt;
-    // peut être remplacé par
     img.setAttribute('src', product.imageUrl);
     img.setAttribute('alt', product.altTxt);
 
-    // img.setAttributeNode(src);
-    // img.setAttributeNode(alt);
-
-
     let titre = document.createElement("h3");
-    // let classeNom = document.createAttribute("class");
-    // classeNom.value = "productName";
-    // peut être remplacé par
+
     titre.classList.add('productName');
 
     titre.textContent = product.name;
 
-    // titre.setAttributeNode(classeNom);
-
     let para = document.createElement("p");
-    // let classePara = document.createAttribute("class");
-    // classePara.value = "productDescription";
-    // peut être remplacé par
+
     para.classList.add('productDescription');
 
     para.textContent = product.description;
-    
-    // para.setAttributeNode(classePara);
 
     lien.append(article);
-    // article.append(img);
-    // article.append(titre);
-    // article.append(para);
-    // peut être remplacé par
+
     article.append(img, titre, para);
 
     return lien;
@@ -145,15 +55,8 @@ function displayProduct(product) {
 
 
 window.addEventListener("DOMContentLoaded", (event) => {
-try{
-  init();
-} 
-catch(err) {
-    alert("ERROR : Veuillez contacter le service client")
-}
 
-
-
+    init();
 });
 
 
