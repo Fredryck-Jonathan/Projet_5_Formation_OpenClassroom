19c66a49ac5bb15c4dae3428d6e95cdd831844cf
Fredryck-Jonathan <102037006+Fredryck-Jonathan@users.noreply.github.com>
2022-08-08 15:23:30 +0200

    changement storage pour un tableau objet

--- front/js/product.js
+++ front/js/product.js
@@ -1,183 +1,203 @@
+/** 
+ * 
+ * 
+ * Recupere l'id et va chercher les données avec l'id recuperer et initialise la fonction createItems
+*/
 function init(){
 
-    id = recupId()
-    
+    id = recupId();
+
 
     let promise01 = fetch("http://localhost:3000/api/products/"+ id);
 
     promise01
     .then((response) => {
-        
+
         const productData = response.json();
 
         productData.then((infos) => { 
-            
-            
-        createItems(infos)
+
+
+        createItems(infos);
+
+        let addToCart = document.getElementById("addToCart");
+
+        addToCart.addEventListener("click", (event) => {
+        createcommand();
+        });
 
 
 
         })
 
-        
-    
     })
 }
 
 
 
-
+/** 
+ * 
+ * 
+ * cette fonction créé les items suite a la recuperation des donnés effectuer en amont et declenche la fonction addColor
+*/
 function createItems(infos){
 
-    let img = document.createElement("img")
+    let img = document.createElement("img");
 
-    img.setAttribute('src', infos.imageUrl)
-    //let src = document.createAttribute("src")
-    //src.value = infos.imageUrl;
-    img.setAttribute('alt', infos.altTxt)
-    //let alt = document.createAttribute("alt")
-    //alt.value = infos.altTxt;
-    //img.setAttributeNode(src)
-    //img.setAttributeNode(alt)
+    img.setAttribute('src', infos.imageUrl);
 
-    let imgcontainer = document.getElementsByClassName("item__img") 
-    imgcontainer[0].append(img)
+    img.setAttribute('alt', infos.altTxt);
 
 
-    //let name = infos.name
-
-    let title = document.getElementById("title")
-    title.textContent = infos.name
-
-    //let price = infos.price
-
-    let pricehtml = document.getElementById("price")
-    pricehtml.textContent = infos.price
-
-    //let description = infos.description
+    let imgcontainer = document.getElementsByClassName("item__img") ;
+    imgcontainer[0].append(img);
 
 
-    let descriptionhtml = document.getElementById("description")
-    descriptionhtml.textContent = infos.description
 
-    console.log(infos)
-    let colors = infos.colors
-    
+    let title = document.getElementById("title");
+    title.textContent = infos.name;
+
+
+
+    let pricehtml = document.getElementById("price");
+    pricehtml.textContent = infos.price;
+
+
+
+
+    let descriptionhtml = document.getElementById("description");
+    descriptionhtml.textContent = infos.description;
+
+    console.log(infos);
+    let colors = infos.colors;
+
 
         for (color in colors){ 
-            
-            addColor(colors)
+
+            addColor(colors);
 
         }
 }
 
+/** 
+ * arguement colors
+ * 
+ * créer les valeurs option et valeur et les ajjoutes au selecteur
+*/
+function addColor(colors){
 
-function addColor(colors){        
-        
+let option = document.createElement("option");
+let valeur = document.createAttribute("value");
+valeur.value = colors[color];
 
-let option = document.createElement("option")
-let valeur = document.createAttribute("value")
-valeur.value = colors[color]
+let text = colors[color];
 
-let text = colors[color]
-
-option.setAttributeNode(valeur)
-option.textContent = text
+option.setAttributeNode(valeur);
+option.textContent = text;
 
 
-let selector = document.getElementById("colors")
-selector.append(option)
+let selector = document.getElementById("colors");
+selector.append(option);
 
 
 
 
 }
 
-let addToCart = document.getElementById("addToCart")
-
-addToCart.addEventListener("click", (event) => {
-    createcommand();
-  });
-  
 
 
 
 
 
+
+/** 
+ * 
+ * 
+ *  valeur qui initie la création de la command
+*/
 function createcommand () {
 
     let recupValeur = recupValue();
 
-    console.log(recupValeur)
+    console.log(recupValeur);
 
 
     if (colorcommand == "" ){
 
-        alert("Veuillez choisir une couleur")
+        alert("Veuillez choisir une couleur");
 
-        
+
     }
    
     else {
 
         if (numbercommand < 1){
 
-        alert("Veuillez choisir une quantité au-dessus de 0")
+        alert("Veuillez choisir une quantité au-dessus de 0");
 
         }
-         
+
         else { 
-            
-            if (localStorage.length != 0 ){
+
+            let ArrayStorage = localStorage.getItem("ArrayStorage");
+
+            console.log(ArrayStorage)
+
+            if (localStorage.length != 0 && ArrayStorage != "[]" ){
+
+              
+              ArrayStorage = JSON.parse(ArrayStorage);
+
+                for( object in ArrayStorage ){
+
+                    console.log(object)
+  
+                    let objectSto = ArrayStorage[object]
                     
-                for( let i = 0; i <= localStorage.length; i++){
+                    let recupValueCommand = recupValue();
 
-                    if (i == localStorage.length){
+                    console.log(objectSto.id,recupValueCommand.idcommand, objectSto.couleur,recupValueCommand.colorcommand)
 
-                    addItem()    
+                        if( objectSto.id === recupValueCommand.idcommand && objectSto.couleur === recupValueCommand.colorcommand){
 
-                    break
+                        changementquantite(objectSto, object);
+
+                        break;
+
+                        }
+
+                        else if( object == (Number(ArrayStorage.length)-1)) {
+
+                        addItem(object)
 
 
-                    }
-
-                    else {  
-                        
-                    
-                    let valueInitSto = initSto(i)
-                    let recupValueCommand = recupValue()
-
-                    console.log(valueInitSto.idstorage)
-
-                        if( valueInitSto.idstorage === recupValueCommand.idcommand && valueInitSto.colorstorage === recupValueCommand.colorcommand){
-
-                        initSto(i)
-
-                        changementquantite();
-                    
-                        break
-           
-    
                         }
                     }
-                }
+                
             }
 
 
             else { 
-        
-            addItem()
+
+            
+
+            addFirstItem();
 
             }
 
         }
     }
 
-    
+
 
 
 }
 
+/** 
+ * 
+ * un id : String
+ * fonction qui sert à aller chercher l'id grace à l'URL
+*/
 function recupId(){
 
 let href = window.location.href;
@@ -187,52 +207,82 @@ let href = window.location.href;
     return id
 }
 
-function recupSto(i){
+/** 
+ * l'argument i
+ * un objet contenant la valeur stokey et stockageObject
+ * 
+*/
+function recupSto(object){
 
-    let stokey = localStorage.key(i);
-    let stoget = localStorage.getItem(stokey);
-    let stockageObject = JSON.parse(stoget);
+    //let stokey = localStorage.key(i);
+    //let stoget = localStorage.getItem(stokey);
+    //let stockageObject = JSON.parse(stoget);
 
-    let stoObj = {
+    console.log(object)
 
-        stokey : stokey,
-        stockageObject: stockageObject
+    ArrayStorage = localStorage.getItem("ArrayStorage");
+
+    ArrayStorage = JSON.parse(ArrayStorage);
+    
+    let elementArray = ArrayStorage[object];
 
 
-    }
 
-        
 
+   
 
     
 
-    return stoObj;
+
+let objRecupSto = {
+
+ArrayStorage : ArrayStorage,
+elementArray : elementArray
+
+
 }
 
-function initSto(i){
 
-    let stockageObject = recupSto(i).stockageObject;
-    let stokey = recupSto(i).stokey;
 
-    console.log(stockageObject)
 
-    let ObjStockage ={
 
-    idstorage : stockageObject.id,
-    colorstorage : stockageObject.couleur,
-    numberstorage : stockageObject.quantite,
-    stockageObject: stockageObject,
-    stokey : stokey
+    return objRecupSto;
+}
+
+/** 
+ * l'argument i
+ * 
+ * à l'aide de la fonction recupSto, on recupere les valeurs stockage objet et on initialise un nouvelle objet qu'on utilisera plu tard
+*/
+function initSto(object){
+
+    let ArrayStorage = recupSto(object).ArrayStorage;
+    let elementArray = recupSto(object).elementArray;
+
+    console.log(elementArray)
+
+    let ObjStockage = {
+
+    idstorage : elementArray.id,
+    colorstorage : elementArray.couleur,
+    numberstorage : elementArray.quantite,
+    elementArray: elementArray,
+    ArrayStorage : ArrayStorage
 
 
     }
 
-    console.log(ObjStockage)
+    console.log(ObjStockage);
 
-    return ObjStockage
+    return ObjStockage;
 
 }
 
+/** 
+ * 
+ * renvoi l'objet value
+ * recupere les valeurs du selecteur et des quantités pour les renvoyer dans un objet
+*/
 function recupValue(){
 
     inputquantity = document.getElementById("quantity");
@@ -240,7 +290,8 @@ function recupValue(){
     id = recupId();
     colorcommand = selector.value;
     numbercommand = inputquantity.value;
-    numberStorage = localStorage.length;
+
+
 
 let objValue = {
 
@@ -248,44 +299,95 @@ let objValue = {
     colorcommand : colorcommand,
 
     numbercommand : numbercommand,
-    
-    numberStorage : numberStorage,
+
 
 }
 
     return objValue;
 }
 
-function changementquantite(){
-    let recupValeur = recupValue()
-    let valeurSto = initSto()
-    numberstorage = Number(recupValeur.numbercommand) + Number(valeurSto.numberstorage);
-    numberstorage = numberstorage.toString();
+/** 
+ * 
+ * 
+ * Recupere les valeurs recupValeur et valeurSto et remplace les quantités
+*/
+function changementquantite(objectSto, object){
 
-    valeurSto.stockageObject.quantite = numberstorage;
+    let arrayStorage = localStorage.getItem("ArrayStorage");
+    arrayStorage = JSON.parse(arrayStorage)
 
-    localStorage.setItem(valeurSto.stokey, JSON.stringify(valeurSto.stockageObject) )
+
+
+
+
+    let recupValeur = recupValue();
+
+    numberstorage = Number(recupValeur.numbercommand) + Number(objectSto.quantite);
+
+    objectSto.quantite = numberstorage;
+
+    console.log(objectSto,objectSto.quantite , object)
+
+    arrayStorage[object] = objectSto
+
+    console.log(arrayStorage)
+
+    localStorage.ArrayStorage = JSON.stringify(arrayStorage);
+    
+    
+    alert("Votre commande a été ajouter")
+
+
+
 
 
 
 }
 
+/** 
+ * 
+ * renvoie une alert 
+ * ajjoute l'objet commandesto au local storage
+*/
 function addItem() {
+
+let objInitSto = initSto(object);
+
+arrayStorage = objInitSto.ArrayStorage;
+
+
+console.log(ArrayStorage)
 
 let recupValeur = recupValue();
 
-let commandesto = {
-    id : recupValeur.idcommand,
-    couleur : recupValeur.colorcommand,
-    quantite : recupValeur.numbercommand
-}
+console.log(recupValeur);
 
-numberStorage = localStorage.length
+arrayStorage.push({id: recupValeur.idcommand, couleur: recupValeur.colorcommand, quantite: recupValeur.numbercommand},);
 
-localStorage.setItem('commande'+ numberStorage , JSON.stringify(commandesto) )
+localStorage.setItem('ArrayStorage', JSON.stringify(arrayStorage));
+
+alert("Votre commande a été ajjouter au panier");
 
 
 }
+
+
+function addFirstItem() {
+
+    let ArrayStorage = [];
+
+    let recupValeur = recupValue();
+    
+    console.log(recupValeur);
+    
+    ArrayStorage.push({id: recupValeur.idcommand, couleur: recupValeur.colorcommand, quantite: recupValeur.numbercommand});
+    
+    localStorage.setItem('ArrayStorage', JSON.stringify(ArrayStorage));
+    
+    alert("Votre commande a été ajouter au panier");
+    
+    
+    }
 
 
 window.addEventListener("DOMContentLoaded", (event) => {
